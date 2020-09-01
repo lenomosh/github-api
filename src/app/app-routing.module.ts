45493f52aec0813d6374a67bf029105c643cfc3d
Lennox <omondilenox@outlook.com>
2020-09-01 11:23:52 +0300

    updated routes, title and forms module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GitProfileComponent} from './Components/git-profile/git-profile.component';
+import {PageNotFoundComponent} from './Components/page-not-found/page-not-found.component';
+import {GithubSearchComponent} from './Components/github-search/github-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: GitProfileComponent},
+  { path: 'search', component: GithubSearchComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TimeElaspePipe } from './Pipes/time-elaspe.pipe';
 import { CountTimePassedPipe } from './Pipes/count-time-passed.pipe';
 import { LoadingColorChangeDirective } from './Directives/loading-color-change.directive';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { GitProfileComponent } from './Components/git-profile/git-profile.component';
 import { GithubSearchComponent } from './Components/github-search/github-search.component';
 import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimeElaspePipe,
     CountTimePassedPipe,
     LoadingColorChangeDirective,
     NavbarComponent,
@@ -24,7 +23,8 @@ import { PageNotFoundComponent } from './Components/page-not-found/page-not-foun
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
