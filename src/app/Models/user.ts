540141737c518b60515e1e8d0a6a7122bad52b15
Lennox <omondilenox@outlook.com>
2020-09-01 08:41:39 +0300

    added users model

---
+++ src/app/Models/user.ts
@@ -0,0 +1,28 @@
+/*tslint:disable:variable-name*/
+
+export class User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  bio: string;
+  name: string;
+  followers: number;
+  following: number;
+  public_repos: number;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  scrore: number;
+}
