f9ea9714ad2593dc6d0f1e66b77a59ddf64dbe3c
Lennox <omondilenox@outlook.com>
2020-09-01 08:40:05 +0300

    added repo model

--- src/app/Models/repository.ts
+++ src/app/Models/repository.ts
@@ -1,2 +1,9 @@
+/*tslint:disable:variable-name*/
 export class Repository {
+  html_url: string;
+  description: string;
+  created_at: Date;
+  name: string;
+  language: string;
+  updated_at: Date;
 }
